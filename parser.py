9f58e7ca53f98541811c165c9ccb6293dfce52bf
Zhen <lizhenpi@gmail.com>
2020-06-01 02:18:12 +0000

    Add support evaluate tsinghua cyclist dataset

--- parser.py
+++ parser.py
@@ -15,15 +15,21 @@ DIFFICULTIES = ['easy', 'moderate', 'hard']
 eval_type = ''
 
 if len(sys.argv)<2:
-    print 'Usage: parser.py results_folder [evaluation_type]'
+    print('Usage: parser.py results_folder [evaluation_type]')
 
-if len(sys.argv)==3:
-    eval_type = sys.argv[2]
-
+cyclist_only = False
+if len(sys.argv)>=3:
+#     eval_type = sys.argv[2]
+    cyclist_arg = sys.argv[2]
+    if (cyclist_arg == 'cyclist-only'):
+        cyclist_only = True
+    
 result_sha = sys.argv[1]
 txt_dir = os.path.join('build','results', result_sha)
 
 for class_name in CLASSES:
+    if cyclist_only and class_name != 'cyclist':
+        continue
     for param in PARAMS:
         print("--{:s} {:s}--".format(class_name, param))
         if eval_type is '':
@@ -32,13 +38,13 @@ for class_name in CLASSES:
             txt_name = os.path.join(txt_dir, 'stats_' + class_name + '_' + param + '_' + eval_type + '.txt')
 
         if not os.path.isfile(txt_name):
-            print txt_name, 'not found'
+            print(txt_name, 'not found')
             continue
 
         cont = np.loadtxt(txt_name)
         # None if this class_name is detected.
         if cont.size == 0:
-          continue
+            continue
 
         averages = []
         for idx, difficulty in enumerate(DIFFICULTIES):
